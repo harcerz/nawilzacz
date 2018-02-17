96126183c3c90e861e562268915a866e994d3793
Mateusz Sych <msych@xplus.pl>
2018-02-18 00:12:56 +0100

    wifiClass

---
+++ EepromDataHandler.cpp
@@ -0,0 +1,77 @@
+#include "EepromDataHandler.h"
+
+bool EepromDataHandlerClass::clearEeprom()
+{
+  for (int i = 0; i < 128; ++i)
+  {
+    EEPROM.write(i, 0);
+  }
+
+  EEPROM.end();
+
+  Serial.println("Eeprom cleared");
+  
+  return true;
+}
+
+String EepromDataHandlerClass::getSsid()
+{
+  return _ssid;
+}
+
+String EepromDataHandlerClass::getPassword()
+{
+  return _password;
+}
+
+bool EepromDataHandlerClass::writeWifiDataToEeeprom(String ssid, String password)
+{
+  Serial.println(ssid);
+  Serial.println(password);
+
+  clearEeprom();
+  EEPROM.begin(512);
+
+  for (int i = 0; i < ssid.length(); ++i)
+  {
+    EEPROM.write(i, ssid[i]);
+  }
+  for (int i = 0; i < password.length(); ++i)
+  {
+    EEPROM.write(64 + i, password[i]);
+  }
+
+  bool success = EEPROM.commit();
+
+  Serial.println("Saved to eeprom");
+  Serial.println(success);
+
+  return success;
+}
+
+
+
+bool EepromDataHandlerClass::readEepromWiFiParameters()
+{
+  Serial.println("Starting eeprom read");
+  for (int i = 0; i < 64; ++i)
+  {
+    _ssid += char(EEPROM.read(i));
+  }
+  for (int i = 64; i < 128; ++i)
+  {
+    _password += char(EEPROM.read(i));
+  }
+  Serial.println(_ssid);
+  Serial.println(_password);
+  Serial.println("Readed from eeprom");
+
+  return true;
+}
+
+void EepromDataHandlerClass::init()
+{
+  EEPROM.begin(512);
+}
+
+EepromDataHandlerClass EepromDataHandler;
---
+++ EepromDataHandler.h
@@ -0,0 +1,34 @@
+// EepromDataHandler.h
+
+#ifndef _EEPROMDATAHANDLER_h
+#define _EEPROMDATAHANDLER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include <EEPROM.h>
+
+class EepromDataHandlerClass
+{
+ private:
+  String _ssid;
+  String _password;
+
+ protected:
+  
+ public:
+   
+   String getSsid();
+   String getPassword();
+   bool clearEeprom();
+   bool writeWifiDataToEeeprom(String ssid, String password);
+   bool readEepromWiFiParameters();
+   void init();
+};
+
+extern EepromDataHandlerClass EepromDataHandler;
+
+#endif
